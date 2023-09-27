5ad91190f8fe8a5bc7e9196dc959c14d6f9a74fe
Yuvraj Soni <demonsroar2172@gmail.com>
2023-09-27 22:26:56 +0530

    init

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import * as serviceWorker from "./serviceWorker";
+// import * as serviceWorker from "./serviceWorker";
 import { GithubProvider } from "./context/context";
 import { Auth0Provider } from "@auth0/auth0-react";
 // dev-he8peygbhod1fskg.us.auth0.com
@@ -21,4 +21,4 @@ root.render(
     </Auth0Provider>
 );
 
-serviceWorker.unregister();
+
